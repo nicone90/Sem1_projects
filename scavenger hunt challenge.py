bbf5f6d005cf31c25efe567631a455dbf8402c0d
nicone90 <152893021+nicone90@users.noreply.github.com>
2025-05-30 09:07:20 -0500

    Add files via upload
    Ways to find something

---
+++ scavenger hunt challenge.py
@@ -0,0 +1,19 @@
+#scavenger hunt
+schools=["Brown University" "Columbia University" "Cornell University" "Dartmouth College" "Harvard University" "University of Pennsylvania" "Princeton University" "Yale University"]#def ivy():
+    for i in range(1):
+        print(schools)
+#main
+ivy()
+ #2
+#Kdot
+a=["Kendrick Lamar", "Kendrick Lamar", "Kendrick Lamar", "Kendrick Lamar", "Kendrick Lamar", "Kendrick Lamar", "Kendrick Lamar", "Kendrick Lamar", "Gracie Abrams", "Tyler, The Creator", "Kendrick Lamar", "Kendrick Lamar", "Lady Gaga", "Gigi Perez" ,"Kendrick Lamar" ,"Kendrick Lamar", "ROSÉ", "Billie Eilish", "Tyler, The Creator", "Jimin", "The Weeknd", "Cynthia Erivo", "Sabrina Carpenter", "Brenda Lee", "Mariah Carey", "Chappell Roan", "Billie Eilish", "Tyler, The Creator", "Bobby Helms", "Gracie Abrams", "Wham!", "Shaboozey", "GloRilla", "Sabrina Carpenter", "Post Malone", "Stromae", "The Kid LAROI", "Teddy Swims", "Sabrina Carpenter", "Oscar Maydon", "Benson Boone" ,"Kendrick Lamar", "The Marías", "Ariana Grande" ,"Ariana Grande", "Morgan Wallen", "Chappell Roan", "The Neighbourhood", "Noah Kahan", "Sabrina Carpenter"]
+x=0
+def count():
+    x=0
+    for i in range(len(a)):
+        if a[i]== "Kendrick Lamar":
+            x = x+1
+    print(x)
+#Main
+count()
+
